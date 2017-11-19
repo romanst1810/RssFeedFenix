fc19937f964d84aa1a2eadf9dd58cb469e9edb2a
romanst1810 <32653023+romanst1810@users.noreply.github.com>
2017-11-20 00:29:03 +0200

    Fixed variables on client side and changes in service
    Fixed variables on client side and changes in service

--- RssFeedToJson/Controllers/RssController.cs
+++ RssFeedToJson/Controllers/RssController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.ServiceModel.Syndication;
 using System.Web.Http;
 using RssService;
-using RssService.Rss;
+
 
 namespace RssFeedToJson.Controllers
 {
     public class RssController : ApiController
     {
         IRssService _service;
+        public string RssUrl { get; set; }
 
         public RssController() : this(new RssFeedService())
         {
@@ -20,15 +22,36 @@ namespace RssFeedToJson.Controllers
         public RssController(IRssService service)
         {
             _service = service;
+            RssUrl = "http://www.economist.com/sections/obituary/rss.xml";
         }
         
-        [HttpPost]
-        public List<RssItem> GetRssFeed(OperationRequest request)
+        [HttpGet]
+        public IEnumerable<SyndicationItem> GetRssFeed()
         {
             try
             {
-                var result = _service.Fetch(request);
-                return result.Result.Channel.Items;
+                var request = new OperationRequest()
+                {
+                    RssFeedUrl = RssUrl
+                };
+                OperationResult or = _service.Fetch(request);
+                SyndicationFeed result = or.Result;
+                return result.Items;
+            }
+            catch (Exception ex)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPost]
+        public SyndicationItem GetRssFeedById(OperationRequest request)
+        {
+            try
+            {
+                OperationResult or = _service.FetchById(request);
+                SyndicationItem result = or.ItemResult;
+                return result;
             }
             catch (Exception ex)
             {
--- RssFeedToJson/Scripts/simpleRssFeed.js
+++ RssFeedToJson/Scripts/simpleRssFeed.js
@@ -6,35 +6,40 @@
         var urlBase = '/api/Rss';
         var dataFactory = {};
 
-        dataFactory.getRssFeed = function (obj, methodName) {
-            return $http.post(urlBase + '/' + methodName, obj);
+        //dataFactory.getRssFeed = function (obj, methodName) {
+        //    return $http.post(urlBase + '/' + methodName, obj);
+        //};
+        dataFactory.getRssFeed = function (methodName) {
+            return $http.get(urlBase + '/' + methodName);
         };
 
+        dataFactory.getRssFeedById = function (obj, methodName) {
+            return $http.post(urlBase + '/' + methodName, obj);
+        };
         return dataFactory;
     }]);
+
     app.filter('unsafe', function ($sce) { return $sce.trustAsHtml; });
+
     app.controller('rssController', ['$scope', 'dataFactory',
        function ($scope, dataFactory) {
            
            $scope.result = "";
-           $scope.status = "";
-           $scope.myStyle = {};
            $scope.exceptionMessageText = ""; 
            
 
            function getInit() {
-               var operationId = "LoadFromUrl";
+               //var operationId = "LoadFromUrl";
                
-               var obj = {
-                   'OperationId': operationId
-               };
-               dataFactory.getRssFeed(obj, 'GetRssFeed')
+               //var obj = {
+               //    'OperationId': operationId
+               //};
+               //dataFactory.getRssFeed(obj, 'GetRssFeed')
+               dataFactory.getRssFeed('GetRssFeed')
                .then(function (response) {
                    $scope.status = 'ok';
                    $scope.result = response.data;
-                   //angular.forEach($scope.result, function (value, key) {
-                   //    value._description = $sce.trustAsHtml(value._description);
-                   //});
+                  
                    }, function (error) {
                    $scope.exceptionMessageText = 'Unable to get rss feed: ' + error.message;
                });
