d5b6c750b888d7d2845f9f406f29cb6109ba2409
romanst1810 <32653023+romanst1810@users.noreply.github.com>
2017-11-20 11:52:52 +0200

    Created Item.html
    Created web api method and service methods for get item .

--- RssFeedToJson/Scripts/simpleRssFeed.js
+++ RssFeedToJson/Scripts/simpleRssFeed.js
@@ -21,20 +21,14 @@
 
     app.filter('unsafe', function ($sce) { return $sce.trustAsHtml; });
 
-    app.controller('rssController', ['$scope', 'dataFactory',
-       function ($scope, dataFactory) {
+    app.controller('rssController', ['$scope', 'dataFactory','$window',
+       function ($scope, dataFactory, $window) {
            
            $scope.result = "";
            $scope.exceptionMessageText = ""; 
            
 
            function getInit() {
-               //var operationId = "LoadFromUrl";
-               
-               //var obj = {
-               //    'OperationId': operationId
-               //};
-               //dataFactory.getRssFeed(obj, 'GetRssFeed')
                dataFactory.getRssFeed('GetRssFeed')
                .then(function (response) {
                    $scope.status = 'ok';
@@ -45,8 +39,46 @@
                });
            };
 
+           $scope.redirectToItem = function (id) {
+               var url = "http://" + $window.location.host + "/item.html?id="+ encodeURIComponent(id);
+               $window.location.href = url;
+           };
 
 
            getInit();
        }]);
+
+    app.controller('rssItemController',['$scope', 'dataFactory','$window',
+       function ($scope, dataFactory, $window) {
+           
+           $scope.item = "";
+           $scope.exceptionMessageText = ""; 
+           
+
+           function getItem() {
+
+               var url_string = $window.location.href;
+               var url = new URL(url_string);
+               var id = url.searchParams.get("id");
+               var itemId = decodeURIComponent(id);
+               
+               var obj = {
+                   'ItemId': itemId
+               };
+               dataFactory.getRssFeedById(obj, 'GetRssFeedById')
+               .then(function (response) {
+                   $scope.status = 'ok';
+                   $scope.item = response.data;
+                  
+               }, function (error) {
+                   $scope.exceptionMessageText = 'Unable to get rss feed item: ' + error.message;
+               });
+           };
+           
+           getItem();
+       }]);
+
+
+
 }());
+
